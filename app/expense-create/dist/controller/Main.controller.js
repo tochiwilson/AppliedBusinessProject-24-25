dd488d616ccaab30062aa3aa4e5ab973762a70c5
Arian Zeqiri <arian.zeqiri@student.ap.be>
2024-11-20 13:30:23 +0000

    Deployen cap applicatie naar SAP BTP

---
+++ app/expense-create/dist/controller/Main.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(e){"use strict";return e.extend("expensecreate.controller.Main",{onInit:function(){}})});
+//# sourceMappingURL=Main.controller.js.map
